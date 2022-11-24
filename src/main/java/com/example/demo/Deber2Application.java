46213c9a5d4636e4ea5347bb9d1b8252225493e0
Reymon <rahidalgob@uce.edu.ec>
2022-11-24 13:33:19 -0500

    Deber2-Tradicional

---
+++ src/main/java/com/example/demo/Deber2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deber2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deber2Application.class, args);
+	}
+
+}
